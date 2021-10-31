7ca058ad9d22b1d00146ad93a69d40dfadda7386
Effort workers <74310343+effortw@users.noreply.github.com>
2021-11-01 02:15:55 +0600

    Update home.js

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ function go(){
   var sites = site.replace(/\./g, "-");
   var hosts = window.location.hostname;
   
-  var fsite = "/p/ip.html#"+(site);
+  var fsite = "/p/ip.html?"+(site);
   
   
   
